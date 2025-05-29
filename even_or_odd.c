df8073e555eb8b006736dc1b146ba05eb2373aab
Mari <ton.email@example.com>
2025-05-29 22:01:47 +0100

    boir

--- even_or_odd.c
+++ even_or_odd.c
@@ -3,13 +3,12 @@
 int main() {
     int num;
 
-    printf("Enter an integer: ");       // en anglais
     scanf("%d", &num);
 
     if (num % 2 == 0) {
-        printf("%d is even.\n", num);   // "even" = pair
+        printf("%d est pair.\n", num);
     } else {
-        printf("%d is odd.\n", num);    // "odd" = impair
+        printf("%d est impair.\n", num);
     }
 
     return 0;
