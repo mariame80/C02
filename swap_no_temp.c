95c43ae3a83a6cdbf15a966a349f2321bf4c0224
Mari <ton.email@example.com>
2025-05-30 02:08:14 +0100

    azerty

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    int b = 10;
+    int a, b;
 
-    printf("Avant l'échange : a = %d, b = %d\n", a, b);
+    scanf("%d %d", &a, &b);
 
     // Échange des valeurs sans variable temporaire
     a = a + b;  // a devient 15
     b = a - b;  // b devient 5 (valeur initiale de a)
     a = a - b;  // a devient 10 (valeur initiale de b)
 
-    printf("Après l'échange : a = %d, b = %d\n", a, b);  
+    printf("Après échange: a = %d, b = %d\n", a, b);  
 
     return 0;
 }
-
-
