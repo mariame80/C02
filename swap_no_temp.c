25a75cdf28163a916cf6683edd3cbfdbe615a980
Mari <ton.email@example.com>
2025-05-30 22:59:07 +0100

    sel

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main() { // fonction principale
     int a, b;
     scanf("%d %d", &a, &b);
     
@@ -8,6 +8,6 @@ int main() {
     b = a - b;
     a = a - b;
     
-    printf("Après échange: a = %d, b = %d\n", a, b);
+    printf("Après échange: a = %d, b = %d", a, b);
     return 0;
 }
