6c974c345b4628712e765a02f7ed54ffea6541ef
Mari <ton.email@example.com>
2025-05-30 22:50:13 +0100

    corrige

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,15 +2,12 @@
 
 int main() {
     int a, b;
-
     scanf("%d %d", &a, &b);
-
-    // Échange des valeurs sans variable temporaire
-    a = a + b;  // a devient 15
-    b = a - b;  // b devient 5 (valeur initiale de a)
-    a = a - b;  // a devient 10 (valeur initiale de b)
-
-    printf("Après échange: a = %d, b = %d\n", a, b);  
-
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    printf("Après échange: a = %d, b = %d\n", a, b);
     return 0;
 }
