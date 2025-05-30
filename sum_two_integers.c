8ade329ed10c49eb611e902419997fab825f4e3b
Mari <ton.email@example.com>
2025-05-30 15:14:35 +0100

    first commit

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int number1, number2, sum;
+    int number1, number2, Somme;
 
     scanf("%d %d", &number1, &number2);
 
-    // Calculate the sum
-    sum = number1 + number2;
+    // Calculate the Somme
+    Somme = number1 + number2;
 
     // Display the sum
-    printf("La somme est : %d\n", sum);
+    printf("La somme est : %d\n", Somme);
 
     return 0;
 }
