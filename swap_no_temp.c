54ede09267a97088a5c8910fcab63362b77f7878
Mari <ton.email@example.com>
2025-05-30 23:08:11 +0100

    selt

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,12 +2,10 @@
 
 int main() { // fonction principale
     int a, b;
-    scanf("%d %d", &a, &b);
-    
+    scanf("%d %d", &a, &b); // il lit les elements
     a = a + b;
     b = a - b;
     a = a - b;
-    
     printf("Après échange: a = %d, b = %d", a, b);
     return 0;
 }
