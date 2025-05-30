1cef35a3e664d5599bf2e5ae9645ef005bc982ce
Mari <ton.email@example.com>
2025-05-31 00:04:59 +0100

    sold

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
-int main() { // fonction principale
-    int a, b;
-    scanf("%d %d", &a, &b); // il lit les elements
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    printf("Après échange: a = %d, b = %d", a, b);
+#include<stdio.h>
+int main()
+{
+    int nb1,nb2;
+    scanf("%d %d", &nb1, &nb2);
+    nb1= nb1 + nb2;
+    nb2= nb1 - nb2;
+    nb1= nb1 - nb2;
+    printf("Après échange : a = %d, b = %d\n", nb1, nb2);
     return 0;
 }
